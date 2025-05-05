dccbd7fefa1de3b83961509d95b24890d623fe5f
Jose chavez <josechavez1796@gmail.com>
2025-05-05 10:20:29 -0500

    proyecto initial

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'games-top',
+  location: 'us-central1'
+};
+
---
+++ src/enviroments/environment.ts
@@ -0,0 +1,19 @@
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyBWgjLYhBBH_E6LaDYnusC1OI1s1-5wzI4",
+    authDomain: "games-top-a642c.firebaseapp.com",
+    projectId: "games-top-a642c",
+    storageBucket: "games-top-a642c.firebasestorage.app",
+    messagingSenderId: "549336143910",
+    appId: "1:549336143910:web:e5c38892acba540fe96ee7",
+    measurementId: "G-PTCNBRT8D7"
+  },
+  nameApp: 'Games top'
+};
